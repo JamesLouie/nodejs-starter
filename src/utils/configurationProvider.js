db4d83ff59a909c25e0d1578a78598f01d08d4a4
James Louie <Jamlouie12@gmail.com>
2019-01-19 21:08:29 -0800

    - Add override for merging environment appsettings

--- src/utils/configurationProvider.js
+++ src/utils/configurationProvider.js
@@ -1,6 +1,17 @@
 // Provider for getting application configurations
 
 var fs = require('fs');
+var _ = require('underscore');
 var appSettings = JSON.parse(fs.readFileSync(__dirname + '/../../configuration/appsettings.json', 'utf-8'));
+
+if (process.env.CURRENT_ENV) {
+    try {
+        var envAppSettings = JSON.parse(fs.readFileSync(__dirname + `/../../configuration/appsettings.${process.env.CURRENT_ENV}.json`, 'utf-8'));
+        _.extend(appSettings, envAppSettings);
+    }
+    catch(exception) {
+        console.log(`[ERROR] Unable to read settings file for ${process.env.CURRENT_ENV} with exception: ${exception}`);
+    }
+}
 
 module.exports = appSettings;
